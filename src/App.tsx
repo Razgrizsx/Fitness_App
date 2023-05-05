347686fd97844e03ee8c8f2ccb1027373d56601f
Razgrizsx <razgrisss41@gmail.com>
2023-05-05 13:37:32 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import NavBar from "@/scenes/navbar"
+import { useState } from 'react'
+
+function App() {
+  const [selectedPage, setSelectedPage] = useState<string>('home')
+
+  return (
+    <div className='App bg-gray-20'>
+      <NavBar
+      selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+    </div>
+  )
+}
+
+export default App
